99744db2155bb7b21604a8ffaa33998d6aafe190
KEY271 <52455240+KEY271@users.noreply.github.com>
2025-12-30 03:41:26 +0900

    Add works

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -2,10 +2,12 @@ export const ui = {
   en: {
     nav_about: "About",
     nav_cv: "CV",
+    nav_works: "Works",
   },
   ja: {
     nav_about: "自己紹介",
     nav_cv: "経歴",
+    nav_works: "研究",
   },
 } as const;
 
