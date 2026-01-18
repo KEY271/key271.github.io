eddc8271abe10504cca2226e6990bfd101dce50c
KEY271 <52455240+KEY271@users.noreply.github.com>
2026-01-19 00:25:18 +0900

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://key271.github.io",
+
   i18n: {
     locales: ["en", "ja"],
     defaultLocale: "en",
@@ -11,7 +14,10 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
   image: {
     responsiveStyles: true,
   },
+
+  integrations: [sitemap()],
 });
